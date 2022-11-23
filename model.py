5f9c4230fbb78fb1b8f8ae96061918aca78a0193
Alagie Sellu <alagiesellu@gmail.com>
2022-11-23 11:44:17 +0000

    first commit

---
+++ model.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import os
+import subprocess
+
+from PIL import Image, ImageOps
+
